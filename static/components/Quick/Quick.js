4ab4fd5697251c3d5aed2da720b4ea1ffcb2451e
tochy97 <tochyegeonu97@gmail.com>
2024-01-09 19:00:53 -0600

    Notification on click added

--- static/components/Quick/Quick.js
+++ static/components/Quick/Quick.js
@@ -5,20 +5,34 @@ function StartQuick() {
     let notification = quick.querySelector(".notification");
 
     closeOptions();
+    return;
 }
 
-function openOptions ()
+function openOptions (type)
 {
     let form = document.forms.main;
-    let widget = form.elements.widget;debugger
+    let widget = form.elements.widget;
     let quickOptions = form.elements.quickOptions;
 
     let chatButton = widget.querySelector(".chatButton");
+    let notificationButton = widget.querySelector(".notificationButton");
     let options = quickOptions.querySelectorAll('a, hr');
 
-    chatButton.style.visibility = "hidden";
+    if (quickOptions.isOpen === true) {
+        closeOptions(type);
+    }
+
     quickOptions.style.visibility = "visible";
     widget.style.marginRight = "100px";
+
+    if (type === "N") {
+        quickOptions.style.borderColor = "rgb(255,0,0)";
+        notificationButton.style.visibility = "hidden";
+    }
+    else {
+        quickOptions.style.borderColor = "rgb(255,255,0)";
+        chatButton.style.visibility = "hidden";
+    }
 
     quickOptions.style.height = "100%";
 
@@ -26,29 +40,39 @@ function openOptions ()
         options[i].style.visibility = "visible";
     }
 
-    console.log("open")
+    quickOptions.isOpen = true;
+    return;
 }
 
-function closeOptions ()
+function closeOptions (type)
 {
     let form = document.forms.main;
-    let widget = form.elements.widget;debugger
+    let widget = form.elements.widget;
     let quickOptions = form.elements.quickOptions;
 
 
     let chatButton = widget.querySelector(".chatButton");
+    let notificationButton = widget.querySelector(".notificationButton");
     let options = quickOptions.querySelectorAll('a, hr');
 
     chatButton.style.visibility = "visible";
+    notificationButton.style.visibility = "visible";
     quickOptions.style.visibility = "hidden";
     widget.style.marginRight = "0px";
 
     quickOptions.style.height = "0%";
 
+    if (type === "N") {
+        chatButton.style.visibility = "visible";
+    }
+    else {
+        notificationButton.style.visibility = "visible";
+    }
 
     for (var i = 0; i < options.length; i++) {
         options[i].style.visibility = "hidden";
     }
 
-    console.log("close")
+    quickOptions.isOpen = false;
+    return;
 }
