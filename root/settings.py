69e57bc45ab21f45f2405f2a122c456b4eb6b8da
Chytoe <tochyegeonu97@gmail.com>
2024-10-08 18:11:48 -0500

    No ES6

--- root/settings.py
+++ root/settings.py
@@ -33,9 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    env('DJANGO_ALLOWED_HOSTS'),
-]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 SECURE_SSL_REDIRECT = False
 
